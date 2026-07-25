539192cf5e17f557ca767267869421cf3120ceab
Robert Gabriel Mihai <roby07g@gmail.com>
2026-07-25 22:19:32 +0200

    feat: the app works, it downloads mp3 files from youtube urls.

--- back/index.js
+++ back/index.js
@@ -3,10 +3,16 @@ import cors from 'cors';
 import ytDlp from 'yt-dlp-exec';
 import ffmpegPath from 'ffmpeg-static';
 import path from 'path';
+import fs from 'fs';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const tempDir = path.join(process.cwd(), 'temp');
+if (!fs.existsSync(tempDir)) {
+    fs.mkdirSync(tempDir);
+}
 
 app.post('/api/convert', async (req, res) => {
     const { url } = req.body;
@@ -16,10 +22,19 @@ app.post('/api/convert', async (req, res) => {
     }
 
     try {
-        console.log('Iniciando conversión y descarga para:', url);
-        const outputFilename = `audio-${Date.now()}.mp3`;
-        const outputPath     = path.join(process.cwd(), outputFilename);
+        console.log('Obteniendo informacion del video...');
 
+        const videoInfo = await ytDlp(url, {
+            dumpSingleJson: true,
+            noPlaylist: true,
+        });
+
+        const title = videoInfo.title || 'audio';
+        const thumbnail = videoInfo.thumbnail || '';
+        const outputFilename = `${Date.now()}.mp3`;
+        const outputPath     = path.join(tempDir, outputFilename);
+
+        console.log(`Iniciando la conversion y descarga para ${title}...`);
         await ytDlp(url, {
             extractAudio: true,
             audioFormat: 'mp3',
@@ -28,18 +43,48 @@ app.post('/api/convert', async (req, res) => {
             noPlaylist: true,
         });
 
+        setTimeout(() => {                          // timeout de 10 min, si nadie ha descargado el archivo se borra solo de la carpeta /temp
+            if (fs.existsSync(outputPath)) {
+                fs.unlink(outputPath, (err) => {
+                    if (!err) console.log(`Archivo expirado eliminado: ${outputFilename}`);
+                });
+            }
+        }, 10 * 60 * 1000);
+
         console.log('Conversion completada con exito!');
         res.json({
             message: 'Conversion exitosa',
-            file: outputFilename
+            title: title,
+            thumbnail: thumbnail,
+            downloadUrl: `http://localhost:3000/api/download/${outputFilename}?title=${encodeURIComponent(title)}` 
         });
     } catch (error) {
         console.error('Error durante la conversion:', error);
-        res.status(500).json({ error: 'Fallo al procesar el video' });
+        res.status(500).json({ error: 'Fallo al procesar el video de Youtube' });
     }
 });
 
-// Servidor en escucha en el puerto 3000
+app.get('/api/download/:filename', (req, res) => {
+    const { filename } = req.params;
+    const customTitle = req.query.title ? `${req.query.title}.mp3` : filename;
+    const filePath = path.join(tempDir, filename);
+
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).json({ error: 'El archivo no existe' });
+    }
+
+    res.download(filePath, customTitle, (err) => {
+        if (err) {
+            console.error('Error enviando el archivo:', err);
+        } else {
+            console.log(`Archivo ${filename} descargado con éxito por el usuario.`);
+            fs.unlink(filePath, (unlinkErr) => {
+                if (unlinkErr) console.error('Error eliminando archivo temporal:', unlinkErr);
+            });
+        }
+    })
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,30 +1,68 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, signal } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+interface ConversionResponse {
+  	message: string;
+  	title: string;
+  	thumbnail: string;
+  	downloadUrl: string;
+}
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [ FormsModule ],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+	selector: 'app-root',
+	standalone: true,
+	imports: [FormsModule],
+	templateUrl: './app.html',
+	styleUrl: './app.css'
 })
 export class App {
-  private http = inject(HttpClient);
-  youtubeUrl: string = ""
+	private http = inject(HttpClient);
+	private cdr = inject(ChangeDetectorRef);
+	
+	youtubeUrl: string = '';
+	isLoading: boolean = false;
+	isDownloaded: boolean = false;
+	videoData: ConversionResponse | null = null;
+	errorMessage: string = '';
 
-  convertURL() {
-    if (!this.youtubeUrl) return;
-    const backendUrl = 'http://localhost:3000/api/convert';
-    const body = { url: this.youtubeUrl };
-    this.http.post(backendUrl, body).subscribe({
-      next: (response) => {
-        console.log('Respuesta recibida del servidor:', response);
-      },
-      error: (err) => {
-        console.error('Error al conectar con el servidor:', err);
-      }
-    });
-  }
+	convertURL() {
+		if (!this.youtubeUrl) return;
+
+		this.isLoading = true;
+		this.videoData = null;
+		this.errorMessage = '';
+
+		const backendUrl = 'http://localhost:3000/api/convert';
+		const body = { url: this.youtubeUrl };
+
+		this.http.post<ConversionResponse>(backendUrl, body).subscribe({
+			next: (response) => {
+				console.log('Respuesta que llega del backend:', response);
+				this.videoData = response;
+				this.isLoading = false;
+				this.cdr.detectChanges();
+			},
+			error: (err) => {
+				this.isLoading = false;
+				this.errorMessage = 'Inténtelo de nuevo o convierta otro video.';
+				console.error('Error:', err);
+				this.cdr.detectChanges();
+			}
+		});
+	}
+
+	downloadAndReset() {
+        setTimeout(() => {
+            this.resetForm();
+            this.cdr.detectChanges();
+        }, 800);
+	}
+
+	resetForm() {
+		this.videoData = null;
+		this.youtubeUrl = '';
+		this.errorMessage = '';
+	}
 
 }
