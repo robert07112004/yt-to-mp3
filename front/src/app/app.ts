850bd1e0afb046b848946f75eb5c7686aff7dc97
Robert Gabriel Mihai <roby07g@gmail.com>
2026-07-25 18:59:34 +0200

    feat: first prototype of the frontend done.

--- front/src/app/app.ts
+++ front/src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ FormsModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('front');
+
+  youtube_url = ""
+  submitted_url = ""
+
+  convertURL() {
+    this.submitted_url = this.youtube_url
+  }
+
 }
